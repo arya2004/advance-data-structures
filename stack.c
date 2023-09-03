05d6ba94edc7b830df04355128b51dfee1082774
arya <arya20j4@gmail.com>
2023-09-03 13:35:51 +0530

    li

---
+++ stack.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+
+#define V 4
+
+typedef struct _Stack{
+    int* data;
+    int top;
+    int current_capacity;
+    int max_capacity;
+}Stack;
+
+
+
+int main() {
+
+
+    return 0;
+}
